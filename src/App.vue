47833935c01b605e08063c965137b6f287d6a8e0
StarDust-Zou <70910029+StarDust-Zou@users.noreply.github.com>
2024-01-12 01:09:11 +0800

    提交格式化代码

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <div>
-
-  </div>
+  <div></div>
 </template>
 
-
-<script setup lang='ts' name='App'>
-for (let i = 0; i < 100; i++){
-  console.log(i);
+<script setup lang="ts" name="App">
+for (let i = 0; i < 100; i++) {
+  console.log(i)
 }
+
+let str = '我是祖国的老花骨朵'
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
