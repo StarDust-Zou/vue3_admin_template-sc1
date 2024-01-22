84d71908b7c283a304c8907391795c244727b6a0
StarDust-Zou <70910029+StarDust-Zou@users.noreply.github.com>
2024-01-23 05:49:09 +0800

    feat: 顶部tab栏静态搭建

---
+++ src/layout/tabbar/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tabbar">
+    <div class="tabbar_left">
+      <!-- 顶部左侧静态 -->
+      <BreadCrumb />
+    </div>
+    <div class="tabbar_right">
+      <Setting />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BreadCrumb from './breadcrumb/index.vue'
+import Setting from './setting/index.vue'
+</script>
+
+<style scoped lang="scss">
+.tabbar {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  background-image: linear-gradient(
+    to right,
+    rgb(187, 155, 155),
+    rgb(235, 205, 205),
+    rgb(206, 119, 119)
+  );
+
+  .tabbar_left {
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+  }
+
+  .tabbar_right {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
