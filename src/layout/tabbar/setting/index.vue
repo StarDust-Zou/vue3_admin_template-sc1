b7e8069cb5a56234112c48d93dc8a3177b4043d8
StarDust-Zou <70910029+StarDust-Zou@users.noreply.github.com>
2024-01-27 04:42:54 +0800

    feat: 全屏模式切换

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -5,7 +5,12 @@
     circle
     @click="updateRefresh"
   ></el-button>
-  <el-button size="small" icon="FullScreen" circle></el-button>
+  <el-button
+    size="small"
+    icon="FullScreen"
+    circle
+    @click="fullScreen"
+  ></el-button>
   <el-button size="small" icon="Setting" circle></el-button>
   <img src="/logo.png" style="width: 24px; height: 24px; margin: 0 10px" />
   <!-- 下拉菜单 -->
@@ -28,9 +33,20 @@
 //获取骨架的小仓库
 import useLayOutSettingStore from '@/store/modules/setting'
 const LayOutSettingStore = useLayOutSettingStore()
-
+//刷新按钮点击回调
 const updateRefresh = () => {
   LayOutSettingStore.refresh = !LayOutSettingStore.refresh
+}
+//全屏按钮点击回调
+const fullScreen = () => {
+  //DOM对象的一个属性:可以用来判断当前是不是全屏模式[全屏:true,不是全屏:false]
+  let full = document.fullscreenElement
+  if (!full) {
+    //利用文档根节点的方法requestFullscreen,实现全屏模式
+    document.documentElement.requestFullscreen()
+  } else {
+    document.exitFullscreen()
+  }
 }
 </script>
 
