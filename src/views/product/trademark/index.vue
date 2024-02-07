e161afd6c42046294c453d5049c67fc2698b569e
StarDust-Zou <70910029+StarDust-Zou@users.noreply.github.com>
2024-02-07 23:47:38 +0800

    feat: 品牌管理模块数据

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -3,15 +3,30 @@
     <!-- 卡片顶部添加品牌按钮 -->
     <el-button type="primary" size="default" icon="Plus">添加品牌</el-button>
     <!-- 表格组件：用于展示已有的品牌数据 -->
-    <el-table style="margin: 10px 0" border>
+    <el-table style="margin: 10px 0" border :data="trademarkArr">
       <el-table-column
         label="序号"
         width="80px"
         align="center"
+        type="index"
       ></el-table-column>
-      <el-table-column label="品牌名称"></el-table-column>
-      <el-table-column label="品牌LOGO"></el-table-column>
-      <el-table-column label="品牌操作"></el-table-column>
+      <!-- table-column:默认展示数据用div -->
+      <el-table-column label="品牌名称">
+        <template #="{ row }">
+          <pre>{{ row.tmName }}</pre>
+        </template>
+      </el-table-column>
+      <el-table-column label="品牌LOGO">
+        <template #="{ row }">
+          <img :src="row.logoUrl" alt="" style="width: 100px; height: 100px" />
+        </template>
+      </el-table-column>
+      <el-table-column label="品牌操作">
+        <template #="{ row, $index }">
+          <el-button type="primary" size="small" icon="Edit"></el-button>
+          <el-button type="primary" size="small" icon="Delete"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <!-- 分页器组件 -->
     <el-pagination
@@ -20,7 +35,7 @@
       :page-sizes="[3, 5, 7, 9]"
       :background="background"
       layout="prev, pager, next, jumper, ->, sizes, total"
-      :total="400"
+      :total="total"
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
     />
@@ -28,12 +43,29 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-
+import { ref, onMounted } from 'vue'
+import { reqHasTrademark } from '@/api/product/trademark'
 //当前页码
 let pageNo = ref<number>(1)
 //每一页展示多少条数据
 let limit = ref<number>(3)
+//存储已有品牌数据总数
+let total = ref<number>(0)
+//存储已有品牌的数据
+let trademarkArr = ref<any>([])
+//获取已有品牌的接口封装为一个函数：在任何情况下获取数据，调用函数即可
+const getHasTrademark = async () => {
+  let result = await reqHasTrademark(pageNo.value, limit.value)
+  if (result.code == 200) {
+    //储存已有品牌总个数
+    total.value = result.data.total
+    trademarkArr.value = result.data.records
+  }
+}
+//组件挂载完毕钩子---发一次请求，获取第一页、一页三个已有品牌数据
+onMounted(() => {
+  getHasTrademark()
+})
 </script>
 
 <style scoped></style>
