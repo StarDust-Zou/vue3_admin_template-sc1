fe47e33d13da5f078933e362713e1d753b28dec8
StarDust-Zou <70910029+StarDust-Zou@users.noreply.github.com>
2024-02-09 21:37:05 +0800

    feat: 添加品牌对话框静态

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,7 +1,9 @@
 <template>
   <el-card class="box-card">
     <!-- 卡片顶部添加品牌按钮 -->
-    <el-button type="primary" size="default" icon="Plus">添加品牌</el-button>
+    <el-button type="primary" size="default" icon="Plus" @click="addTrademark">
+      添加品牌
+    </el-button>
     <!-- 表格组件：用于展示已有的品牌数据 -->
     <el-table style="margin: 10px 0" border :data="trademarkArr">
       <el-table-column
@@ -23,7 +25,12 @@
       </el-table-column>
       <el-table-column label="品牌操作">
         <template #="{ row, $index }">
-          <el-button type="primary" size="small" icon="Edit"></el-button>
+          <el-button
+            type="primary"
+            size="small"
+            icon="Edit"
+            @click="updateTrademark"
+          ></el-button>
           <el-button type="primary" size="small" icon="Delete"></el-button>
         </template>
       </el-table-column>
@@ -41,6 +48,34 @@
       :total="total"
     />
   </el-card>
+  <!-- 对话框组件:在添加品牌与修改已有品牌的业务时候使用结构 -->
+  <!-- v-model:属性用户控制对话框的显示与隐藏的 true:显示 false:隐藏 -->
+  <el-dialog v-model="dialogFormVisible" title="添加品牌">
+    <el-form style="width: 80%">
+      <el-form-item label="品牌名称" label-width="80px">
+        <el-input placeholder="请您输入品牌的名称"></el-input>
+      </el-form-item>
+      <el-form-item label="品牌LOGO" label-width="80px">
+        <el-upload
+          class="avatar-uploader"
+          action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon">
+            <Plus />
+          </el-icon>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+    <!-- 具名插槽:footer -->
+    <template #footer>
+      <el-button type="primary" size="default" @click="cancel">取消</el-button>
+      <el-button type="primary" size="default" @click="confirm">确定</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup lang="ts">
@@ -58,6 +93,8 @@ let limit = ref<number>(3)
 let total = ref<number>(0)
 //存储已有品牌的数据
 let trademarkArr = ref<Records>([])
+// 控制对话框显示与隐藏
+let dialogFormVisible = ref<boolean>(false)
 //获取已有品牌的接口封装为一个函数：在任何情况下获取数据，调用函数即可
 const getHasTrademark = async (pager = 1) => {
   pageNo.value = pager
@@ -86,9 +123,47 @@ onMounted(() => {
 //这个自定义事件，分页器组件会将下拉菜单选择数据返回
 const sizeChange = () => {
   //当前每页数据量发生变化的时候，当前页码归一
-
   getHasTrademark()
+}
+//添加品牌按钮回调
+const addTrademark = () => {
+  //对话框显示
+  dialogFormVisible.value = true
+}
+//修改已有品牌按钮回调
+const updateTrademark = () => {
+  //对话框显示
+  dialogFormVisible.value = true
+}
+//对话框底部取消按钮
+const cancel = () => {
+  //对话框隐藏
+  dialogFormVisible.value = false
+}
+const confirm = () => {
+  dialogFormVisible.value = false
 }
 </script>
 
-<style scoped></style>
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+</style>
