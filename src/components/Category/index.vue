e9b1bf67be39bc995a1d2bfffbf0dc377950ab7e
StarDust-Zou <70910029+StarDust-Zou@users.noreply.github.com>
2024-02-21 23:46:24 +0800

    feat: 属性管理静态搭建

---
+++ src/components/Category/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-card>
+    <el-form inline>
+      <el-form-item label="一级分类">
+        <el-select style="width: 240px">
+          <el-option label="北京" value="北京"></el-option>
+          <el-option label="上海" value="上海"></el-option>
+          <el-option label="广州" value="广州"></el-option>
+          <el-option label="深圳" value="深圳"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="二级分类">
+        <el-select style="width: 240px">
+          <el-option label="北京" value="北京"></el-option>
+          <el-option label="上海" value="上海"></el-option>
+          <el-option label="广州" value="广州"></el-option>
+          <el-option label="深圳" value="深圳"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="三级分类">
+        <el-select style="width: 240px">
+          <el-option label="北京" value="北京"></el-option>
+          <el-option label="上海" value="上海"></el-option>
+          <el-option label="广州" value="广州"></el-option>
+          <el-option label="深圳" value="深圳"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 //引入项目中全部的全局组件
 import SvgIcon from './SvgIcon/index.vue'
+import Category from './Category/index.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 //全局对象
-const allGlobalComponent = { SvgIcon }
+const allGlobalComponent = { SvgIcon, Category }
 //对外暴露插件对象
 export default {
   //务必叫做install方法
