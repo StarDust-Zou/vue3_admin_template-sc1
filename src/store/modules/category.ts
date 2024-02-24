53edf69a897fe2f0b6f3aed8e8afc0a850d0309e
StarDust-Zou <70910029+StarDust-Zou@users.noreply.github.com>
2024-02-24 22:11:53 +0800

    feat: 分类数据的ts类型

--- src/store/modules/category.ts
+++ src/store/modules/category.ts
@@ -1,8 +1,10 @@
 //商品分类全局组件的小仓库
 import { defineStore } from 'pinia'
 import { reqC1 } from '@/api/product/attr'
+import type { CategoryResponseData } from '@/api/product/attr/type'
+import { CategoryState } from './type/type'
 let useCategoryStore = defineStore('Category', {
-  state: () => {
+  state: (): CategoryState => {
     return {
       //存储一级分类的数据
       c1Arr: [],
@@ -13,7 +15,7 @@ let useCategoryStore = defineStore('Category', {
   actions: {
     //获取一级分类的方法
     async getC1() {
-      let result: any = await reqC1()
+      let result: CategoryResponseData = await reqC1()
       if (result.code == 200) {
         this.c1Arr = result.data
       }
